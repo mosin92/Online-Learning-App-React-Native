e846a8a5e76c69f44c0242be3dabe2f3d8d12dd5
Mohsin <55811423+mosin92@users.noreply.github.com>
2022-11-06 14:47:35 +0500

    inital commit & setup all assets , fonts ,images

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Dashboard } from './screens';
+
+const App = () => {
+
+  return (
+    <Dashboard />
+  );
+};
+
+
+
+export default App;
